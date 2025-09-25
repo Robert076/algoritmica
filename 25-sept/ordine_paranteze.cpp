d64769521f562ad92297fff087da56b7764852ee
Robert076 <robertberes06@gmail.com>
2025-09-25 19:11:45 +0300

    paranteze

--- 25-sept/ordine_paranteze.cpp
+++ 25-sept/ordine_paranteze.cpp
@@ -19,6 +19,40 @@ int main()
         cout << "Ma execut de 3 ori";
     cout << "Nu ma execut de 3 ori";
 
+    int n = 50;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Acest cod se executa de N ori";
+        cout << "De exemplu, daca am N numere in vector";
+        cout << "Pot pune v[i] iar i va fi 1, 2, 3..., N (50 in cazul nostru)";
+    }
+    cout << "Dar de aici incolo nu se va mai executa codul de 50 de ori";
+
+    // sau un alt exemplu:
+    // for (int i = 1; i <= n; i++)
+    //      if(Numarul meu este par)
+    //          par = 1
+    //          pozitie_par = i // mare atentie, linia asta nu se executa in if, se va executa dupa for
+
+    // dar se poate rezolva asa:
+    // sau un alt exemplu:
+    // for (int i = 1; i <= n; i++)
+    //      if(Numarul meu este par)
+    //      {
+    //          par = 1
+    //          pozitie_par = i
+    //      }
+
+    // mare atentie sa nu puneti
+    // dar se poate rezolva asa:
+    // sau un alt exemplu:
+    // for (int i = 1; i <= n; i++)
+    // {
+    //      if(Numarul meu este par)
+    //          par = 1
+    //          pozitie_par = i  // daca pun asa, pozitie_par = i pentru fiecare i de la 1 la 50 (nu este in acolada de if)
+    // }
+
     // tinem minte, mereu se executa prima conditie de sub for/while/if/dowhile
     // daca vrem sa executam mai multe lucruri, punem acolada
 
